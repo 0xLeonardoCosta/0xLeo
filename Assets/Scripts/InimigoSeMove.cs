10992e6932441660cf470921c773b55fe4937421
Leonardo Costa <leonardomcosta13@gmail.com>
2023-09-15 17:55:36 -0300

    Inimigo Morrendo 15/09
    Atividade feita em aula

---
+++ Assets/Scripts/InimigoSeMove.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class InimigoSeMove : MonoBehaviour
+{
+    NavMeshAgent _agent;
+    Animator _animator;
+    [SerializeField] Transform _alvo;
+    [SerializeField] Transform _player;
+    [SerializeField] Transform[] _pos;
+
+    //---------------------------------------------
+
+    float _distancia;
+    float _distanciaPlayer;
+    [SerializeField] float _distanciaPatrulhar;
+    [SerializeField] float _speedPatrulha;
+    [SerializeField] float _speedPerseguicao;
+
+    [SerializeField] float _speedAnim;
+
+    Vector3 _speedAgent;
+
+    int _numberM;
+
+    bool _seguirPlayer;
+
+    [SerializeField] bool _hitCheck;
+
+    //---------------------------------------------
+
+    float _checkTime;
+    [SerializeField] float _timeLimit;
+
+    Hit _hit;
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+        _hit = GetComponent<Hit>();
+        _checkTime = _timeLimit;
+    }
+
+
+    void Update()
+    {
+        if (!_hitCheck)
+        {
+            Movimento();
+        }
+        else
+        {
+            Hit(true);
+        }
+        _speedAgent = _agent.velocity;
+        Animacoes();
+
+        if (_hitCheck)
+        {
+            _checkTime -= Time.deltaTime;
+            if (_checkTime < 0)
+            {
+                Hit(false);
+                _hitCheck = false;
+                _checkTime = _timeLimit;
+            }
+        }
+    }
+
+    void Patrulhar()
+    {
+        if (_distancia < _distanciaPatrulhar && _numberM == 0)
+        {
+            //_alvo = _pos[1];
+            _numberM = 1;
+        }
+        else if (_distancia < _distanciaPatrulhar && _numberM == 1)
+        {
+            //_alvo = _pos[0];
+            _numberM = 0;
+        }
+        _alvo = _pos[_numberM];
+    }
+
+    void Movimento()
+    {
+        //_agent.destination = _alvo.position;
+        _agent.SetDestination(_alvo.position);
+        _distancia = _agent.remainingDistance;
+        _distanciaPlayer = Vector3.Distance(transform.position, _player.position);
+        if (_distanciaPlayer < 12)
+        {
+            _seguirPlayer = true;
+            _agent.speed = _speedPerseguicao;
+        }
+        else
+        {
+            _seguirPlayer = false;
+            _agent.speed = _speedPatrulha;
+        }
+
+        if (_distanciaPlayer < 4 && _seguirPlayer == true)
+        {
+            _agent.speed = 0;
+        }
+
+        if (!_seguirPlayer)
+        {
+            Patrulhar();
+            _agent.SetDestination(_alvo.position);
+        }
+        else
+        {
+            _agent.SetDestination(_player.position);
+        }
+    }
+
+    void Animacoes()
+    {
+        _speedAgent = _agent.velocity;
+        _speedAnim = Mathf.Abs(_speedAgent.x) + Mathf.Abs(_speedAgent.z);
+        _animator.SetFloat("Speed", _speedAnim);
+        _animator.SetBool("hitmorte", _hitCheck);
+    }
+
+    void Morte()
+    {
+
+    }
+    void Hit(bool on)
+    {
+        if (on)
+        {
+            _agent.velocity = new Vector3(0, 0, 0);
+            gameObject.GetComponent<CapsuleCollider>().enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+        else
+        {
+            gameObject.GetComponent<CapsuleCollider>().enabled = true;
+            GetComponent<Rigidbody>().isKinematic= false;
+            
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("AtaqueMili"))
+        {
+            _hitCheck = true;
+        }
+    }
+}
