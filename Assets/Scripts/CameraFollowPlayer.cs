4185f68367a86ae3f263fc010a66924aafaff2fd
Leonardo Costa <leonardomcosta13@gmail.com>
2023-10-17 18:12:17 -0300

    Nunca mais eu vou dormir

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
-    public Transform target;
+    //public Transform target;
+    public GameObject target;
     public float smoothing = 5f;
-    Vector3 offset;
+    //Vector3 offset;
+    Vector3 offset = new Vector3 (0, 5, -13);
 
     void Start()
     {
-        offset = transform.position - target.position;
+        //offset = transform.position - target.position;
     }
 
     void LateUpdate()
     {
-        Vector3 targetCamPos = target.position + offset;
-        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        //Vector3 targetCamPos = target.position + offset;
+        //transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        transform.position = target.transform.position + offset;
+        transform.position = Vector3.Lerp()
     }
 }
